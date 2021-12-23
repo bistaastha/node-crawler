5a92b1cafc8d5d3a219af2b8a66d9993d4100432
Aastha Bist <abist119@gmail.com>
2021-12-23 22:22:46 +0530

    Add scraping for initial page
    Signed-off-by: Aastha Bist <abist119@gmail.com>

---
+++ index.js
@@ -0,0 +1,7 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+//const pretty = require('pretty');
+const runApp = require('./lib/app');
+const url = 'https://stackoverflow.com/questions';
+
+runApp();
