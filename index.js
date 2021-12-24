007d5e9a14989159e95e19e575f7ceb8bf5ad5f9
Aastha Bist <abist119@gmail.com>
2021-12-24 23:34:15 +0530

    Add concurrency tests for existing scripts
    Signed-off-by: Aastha Bist <abist119@gmail.com>

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const runApp = require("./lib/app");
-const { exec } = require('child_process');
+const { exec } = require("child_process");
 const r = require("rethinkdb");
-runApp();
+// const pLimit = require("p-limit");
 
+// const limit = pLimit(5);
+
+// const functionList = [
+//     limit(() => runApp()),
+//     limit(() => runApp()),
+//     limit(() => runApp()),
+//     limit(() => runApp()),
+//     limit(() => runApp())
+// ];
+
+// Promise.all(functionList).then((res) => console.log(res));
+runApp();
 process.on("SIGINT", () => {
   console.log("caughtInterruptSignal");
-  const script = 'rethinkdb export -e scraperdb.questions --format csv --fields path,referenceCount,upvotes,answers';
+  const script =
+    "rethinkdb export -e scraperdb.questions --format csv --fields path,referenceCount,upvotes,answers";
   exec(script);
   process.exit();
 });
